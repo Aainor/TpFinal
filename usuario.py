b6d7ca6b2387b0b54460fdf22562655c41813e1c
Faba50 <booxdoor_111@hotmail.com>
2025-06-16 15:14:13 -0300

    correcion de error

---
+++ usuario.py
@@ -0,0 +1,11 @@
+from nicegui import ui
+
+class Usuario:
+    def __init__(self, id, nombre, email, contraseña,historial):
+        self.id = id
+        self.nombre = nombre
+        self.email = email
+        self.contraseña = contraseña
+        self.historial = historial
+    def __str__(self):
+        return f"Nombre: {self.nombre}, Email: {self.email}, Contraseña: {self.contraseña}, Historial: {self.historial}"
