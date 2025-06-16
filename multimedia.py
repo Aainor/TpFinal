d427922ce9853a118a9d963504f3aa5a910688ca
Faba50 <booxdoor_111@hotmail.com>
2025-06-16 15:29:54 -0300

    creacion de la clase multimedia con herencia en peli

---
+++ multimedia.py
@@ -0,0 +1,9 @@
+class Multimedia:
+    def __init__(self, nombre, descripcion, categoria):
+        self._nombre = nombre
+        self._descripcion = descripcion
+        self._categoria = categoria
+
+    def reproducir():
+        print ("reproduciendo")
+        
