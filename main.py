b1496d5998bbcda755c824a69df0e65b9ad32afd
MrLem0n <lisandro.m.yo@gmail.com>
2025-06-16 16:18:55 -0300

    dmo

--- main.py
+++ main.py
@@ -2,25 +2,35 @@ from nicegui import ui, ui_run
 import mysql.connector 
 import os
 from dotenv import load_dotenv
+from serie import Serie
+
+contenidos=[]
 load_dotenv()
+
+
+
+
 @ui.page("/")
 def inicio():
-    ui.label("bienvenido")
-
-
-@ui.page("/contenidos")
-def contenido():
     conexion = mysql.connector.connect(
-        host='127.0.0.1',
-        port=3306,
-        user=os.getenv("USER"),
-        password='',
-        database=os.getenv("DB")
-    )
-    ui.label("contenidos:")
+            host='127.0.0.1',
+            port=3306,
+            user=os.getenv("USER"),
+            password='',
+            database=os.getenv("DB")
+        )
+
     with conexion.cursor() as cursor:
-        cursor.execute("SELECT * FROM contenido")
-        contenidos = cursor.fetchall()
-        for contenido in contenidos:
-            ui.label(contenido)
-    ui.run()
+            cursor.execute("SELECT * FROM contenidos")
+            resultados_db = cursor.fetchall()
+            # Procesar los resultados sin modificar la lista original durante la iteración
+            for contenido in resultados_db:
+                contenido=Serie(
+                nombre=contenido[4],
+                descripcion=contenido[2],
+                categoria=contenido[3],
+                episodios=contenido[0],
+                temporadas=contenido[5]
+                )
+                ui.label(contenido.getnombre())
+ui.run()
--- serie.py
+++ serie.py
@@ -5,8 +5,20 @@ class Serie(Multimedia):
         super().__init__(nombre, descripcion, categoria)
         self._episodios = episodios
         self._temporadas = temporadas
-        
+    def getnombre(self):
+        return self._nombre
     def reproducir(self):
         h = super().reproducir()
-        f"{h} {self.nombre}"
-
+        f"{h} {self.nombre}" #Y ESTE METODO DE VIRGO MOMO?
+series=[]
+def instanciarS(y):
+    for x in y:
+        content= Serie(
+            nombre=x["nombre"],
+            descripcion=x["descripcion"],
+            categoria=x["categoria"],
+            episodios=x["episodios"],
+            temporadas=x["temporadas"]
+        )
+        series.append(content)
+    return series
